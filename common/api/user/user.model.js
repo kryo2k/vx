a978e12b93e5857e853fa5ed7b78549908794f3c
Hans Doller <hans@w3forge.org>
2016-02-29 20:22:23 -0500

    Updates to include secure cryptography support at the user level.

--- common/api/user/user.model.js
+++ common/api/user/user.model.js
@@ -3,7 +3,16 @@
 var
 crypto  = require('crypto'),
 mongoose = require('mongoose'),
+owasp = require('owasp-password-strength-test'),
 Schema = mongoose.Schema;
+
+owasp.config({
+  allowPassphrases       : true,
+  maxLength              : 128,
+  minLength              : 8,
+  minPhraseLength        : 20,
+  minOptionalTestsToPass : 4
+});
 
 const
 keySerializeAs  = 'hex',
@@ -16,23 +25,88 @@ UserSchema = new Schema({
   name: String,
   email: {
     type: String,
-    required: true
+    required: true,
+    lowercase: true,
+    unique: true // enforces uniqueness, but throws error.
   },
   created: {
     type: Date,
     required: true,
     default: Date.now
   },
-  _privateKey: {
-    type: String,
-    validate: {
-      validator: function (v) {
-        return /^[0-9a-f]{65}$/.test(v);
-      },
-      message: '{VALUE} is not a valid key.'
-    }
-  }
+  _encryptedPassword: String,
+  _privateKey: String
 });
+
+UserSchema
+.path('email')
+.validate(function (value, respond) { // unique check does NOT work for User.create({}, {}, ...)
+  this.checkUniqueEmail(value).then(respond);
+}, 'The specified email address is already in use.');
+
+UserSchema.virtual('password')
+  .get(function () {
+    if (!this.privateKey || !this._encryptedPassword) { // not allowed to set if _privateKey isn't available
+      return null;
+    }
+
+    return this.decrypt(this.publicKey, this._encryptedPassword);
+  })
+  .set(function (val) {
+    delete this._lastPasswordErrors;
+
+    var results;
+    if((results = this.constructor.passwordTest(val)) !== true) { // invalid/insecure password
+      this.invalidate('password', this._lastPasswordErrors = results);
+    }
+
+    if(!val) {
+      return;
+    }
+
+    var pkey = this.privateKey;
+
+    if (!pkey && this.isNew) { // hasn't set private key yet. reset/regenerate private key
+      this.resetPrivateKey();
+      pkey = this.privateKey;
+    }
+
+    this._plaintextPassword = val;
+    this._encryptedPassword = pkey ? this.encrypt(this.publicKey, val) : false;
+  });
+
+UserSchema.virtual('passwordConfirm')
+  .get(function () {
+    return this._passwordConfirm;
+  })
+  .set(function (val) {
+    this._passwordConfirm = val;
+  });
+
+UserSchema
+  .path('_encryptedPassword')
+  .validate(function(epw) {
+
+    // console.log('Validating _encryptedPassword:');
+    // console.log('-- argument   value: %j', epw);
+    // console.log('-- plain-text value: %j', this._plaintextPassword);
+    // console.log('-- confirm    value: %j', this._passwordConfirm);
+    // console.log('-- encrypted  value: %j', this._encryptedPassword);
+    // console.log('--      last errors: %j', this._lastPasswordErrors);
+
+    if(this._lastPasswordErrors || this._plaintextPassword || this._passwordConfirm) {
+      if(this._lastPasswordErrors) {
+        this.invalidate('password', this._lastPasswordErrors);
+      }
+      else if(this._plaintextPassword !== this._passwordConfirm) {
+        this.invalidate('passwordConfirm', 'Password confirmation does not match password.');
+      }
+    }
+
+    if(this.isNew && !this._plaintextPassword) {
+      this.invalidate('password', 'Password is required');
+    }
+  }, 'Password cannot be blank');
 
 UserSchema.virtual('privateKey')
   .get(function () {
@@ -53,12 +127,49 @@ UserSchema.virtual('publicKey')
 UserSchema.pre('save', function(next) {
   if (!this.isNew) return next();
   if (!this.privateKey) { // create a new private key for this user, only if not previously set
-    this.privateKey = this.model('User').createPrivateKey();
-    next();
+    this.resetPrivateKey();
   }
+  if (!this._encryptedPassword) {
+    return next(new Error('Password was not defined for user.'));
+  }
+
+  next();
 });
 
 UserSchema.statics = {
+  checkUniqueEmail: function (email, existingUser, cb) {
+    var
+    promise = new mongoose.Promise(cb),
+    eq = existingUser._id.equals.bind(existingUser._id);
+
+    this.findOne({ email: email }, function (err, user) {
+      if(err) {
+        return promise.error(err);
+      }
+
+      if(user) {
+        if(eq(user._id)) {
+          return promise.complete(true);
+        }
+
+        return promise.complete(false);
+      }
+
+      promise.complete(true);
+    });
+
+    return promise;
+  },
+  passwordTest: function (password) {
+    var
+    result = owasp.test(password);
+
+    if(!result.strong) {
+      return result.errors;
+    }
+
+    return true;
+  },
   createECDH: function (privateKey) {
     var ecdh = crypto.createECDH(keyAlgorithm);
 
@@ -83,11 +194,14 @@ UserSchema.statics = {
 };
 
 UserSchema.methods = {
+  checkUniqueEmail: function (email, cb) {
+    return this.constructor.checkUniqueEmail(email, this, cb);
+  },
   createCipher: function (publicKey) {
-    return this.model('User').createCipher(this.computeSecret(publicKey));
+    return this.constructor.createCipher(this.computeSecret(publicKey));
   },
   createDecipher: function (publicKey) {
-    return this.model('User').createDecipher(this.computeSecret(publicKey));
+    return this.constructor.createDecipher(this.computeSecret(publicKey));
   },
   createECDH: function () {
     var pkey = this.privateKey;
@@ -96,7 +210,7 @@ UserSchema.methods = {
       throw new Error('Private key has not been set for this user.');
     }
 
-    return this.model('User').createECDH(pkey);
+    return this.constructor.createECDH(pkey);
   },
   computeSecret: function (publicKey) {
     return this.createECDH().computeSecret(publicKey);
@@ -109,6 +223,10 @@ UserSchema.methods = {
     var decipher = this.createDecipher(publicKey);
     outputAs = outputAs || 'utf8';
     return decipher.update(message, cipSerializeAs, outputAs) + decipher.final(outputAs);
+  },
+  resetPrivateKey: function () { // unsaved!!
+    this._privateKey = this.constructor.createPrivateKey();
+    return this;
   }
 };
 
--- test-code/test-user.js
+++ test-code/test-user.js
@@ -13,6 +13,11 @@ User = require('../common/api/user/user.model');
 function error(err) {
   if(err) {
     console.error('ERROR:', err.stack||err);
+
+    if(err.errors) {
+      console.error(err.errors);
+    }
+    // db.disconnect();
   }
 
   return err;
@@ -23,19 +28,33 @@ User.find({}).remove(function (err) {
     return error(err);
   }
 
+  var
+  pwEasy = '123456',
+  pwHard = 'my incredibly hard password';
+
   User.create({
     name: 'Alice',
-    email: 'alice@alicesite.com'
+    email: 'alice@alicesite.com',
+    password: pwHard,
+    passwordConfirm: pwHard
   }, {
     name: 'Bob',
-    email: 'bob@bobsite.com'
+    email: 'bob@bobsite.com',
+    password: pwHard,
+    passwordConfirm: pwHard
   },{
     name: 'Charlie',
-    email: 'charlie@middleman.com'
+    email: 'charlie@middleman.com',
+    password: pwHard,
+    passwordConfirm: pwHard
   }, function (err, d1, d2, d3) {
     if(err) {
       return error(err);
     }
+
+    // console.log('Alice Password:',   d1.password);
+    // console.log('Bob Password:',     d2.password);
+    // console.log('Charlie Password:', d3.password);
 
     [d1, d2, d3].forEach(function(user, index){
       console.log('user %s, public key: %j', user._id, user.publicKey.toString('hex'));
@@ -46,7 +65,7 @@ User.find({}).remove(function (err) {
     pubBob     = d2.publicKey,
     pubCharlie = d3.publicKey;
 
-    var
+    var // sign to other user testing
     aliceMessage = d1.encrypt(pubBob, 'Hello Bob, this is a secure message from Alice.'),
     bobDecoded   = d2.decrypt(pubAlice, aliceMessage),
     bobMessage   = d2.encrypt(pubAlice, 'Hello Alice, this is a secure message from Bob.'),
@@ -61,17 +80,19 @@ User.find({}).remove(function (err) {
     console.log('Charlie intercepted (alice -> bob): %j', d3.decrypt(pubAlice, aliceMessage));
     console.log('Charlie intercepted (bob -> alice): %j', d3.decrypt(pubBob, bobMessage));
 
-    // alice wants to sign a secret for herself only.
-
-    var
-    aliceSecret = d1.encrypt(pubAlice, 'The key to the door is under the rock.');
+    var // self signed secret testing
+    aliceSecret   = d1.encrypt(pubAlice,   'The key to the door is under the rock.'),
+    bobSecret     = d2.encrypt(pubBob,     'Lost 250,000 in vegas, dont tell wife.'),
+    charlieSecret = d3.encrypt(pubCharlie, 'Magical powers can be obtained by eating mangos.');
 
     console.log('------------------------------')
-    console.log('Alice secret: %j (%s)', aliceSecret, d1.decrypt(pubAlice, aliceSecret));
+    console.log('  Alice secret: %j (%s)', aliceSecret,   d1.decrypt(pubAlice,   aliceSecret));
+    console.log('    Bob secret: %j (%s)', bobSecret,     d2.decrypt(pubBob,     bobSecret));
+    console.log('Charlie secret: %j (%s)', charlieSecret, d3.decrypt(pubCharlie, charlieSecret));
     console.log('------------------------------')
     console.log('Charlie intercepted (alice -> alice): %j', d3.decrypt(pubAlice, aliceSecret));
+    console.log('Charlie intercepted     (bob -> bob): %j', d3.decrypt(pubBob,   bobSecret));
 
-    //
     db.disconnect();
   });
 });
